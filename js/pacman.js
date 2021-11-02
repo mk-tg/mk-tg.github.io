0603e1455b7a1e112add89871893df52272ef124
mk-tg <67037078+mk-tg@users.noreply.github.com>
2021-11-02 14:06:21 -0700

    added details to project pages

--- js/pacman.js
+++ js/pacman.js
@@ -1,4 +1,6 @@
-var pos = 0;
+var pacManContainer = document.getElementById('pacman-container');
+var pos = pacManContainer.offsetLeft;
+
 const pacArray = [
     ['./images/PacMan1.png', './images/PacMan2.png'],
     ['./images/PacMan3.png', './images/PacMan4.png']
@@ -10,23 +12,24 @@ function Run() {
     let img = document.getElementById("PacMan");
     let imgWidth = img.width
     focus = (focus + 1) % 2;
-    direction = checkPageBounds(direction, imgWidth);
+    direction = checkBoxBounds(direction, imgWidth);
     img.src = pacArray[direction][focus];
     if (direction) {
-        pos -= 20;
+        pos -= 10;
         img.style.left = pos + "px";
     } else {
-        pos += 20;
+        pos += 10;
         img.style.left = pos + 'px';
     }
 
-    setTimeout(Run, 100);
+    setTimeout(Run, 75);
 }
 
-function checkPageBounds(direction, imgWidth) {
-    let pageWidth = document.getElementById('pacman-container');
-    if (direction == 0 && pos + imgWidth > pageWidth) direction = 1;
-    if (direction == 1 && pos < 0) direction = 0;
+function checkBoxBounds(direction, imgWidth) {
+    let containerWidth = pacManContainer.offsetWidth + pacManContainer.offsetLeft;
+    console.log(containerWidth);
+    if (direction == 0 && pos + imgWidth > containerWidth) direction = 1;
+    if (direction == 1 && pos < pacManContainer.offsetLeft) direction = 0;
 
     return direction;
 }
